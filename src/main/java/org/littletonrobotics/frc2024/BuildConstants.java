8b12b47c821bbb5cd904277ee22cf4f283f44283
Ishan1522 <iacharyagangopadhyay@students.chccs.k12.nc.us>
2024-06-12 13:52:45 -0400

    started vision io (copied from 2910)

--- src/main/java/org/littletonrobotics/frc2024/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2024/BuildConstants.java
@@ -5,14 +5,14 @@ package org.littletonrobotics.frc2024;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2024-offseason-robot-code-1";
+  public static final String MAVEN_NAME = "offseason-robot-code-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 362;
-  public static final String GIT_SHA = "7cb3794f9299723a22e5d29aaeccdac11ef328e4";
-  public static final String GIT_DATE = "2024-05-03 14:44:01 EDT";
+  public static final int GIT_REVISION = 365;
+  public static final String GIT_SHA = "c2731bc6a0a745628ba1a072e57eb3ff9789c822";
+  public static final String GIT_DATE = "2024-05-21 10:27:23 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-05-05 14:01:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1714932106184L;
+  public static final String BUILD_DATE = "2024-06-12 12:45:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1718210737034L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
