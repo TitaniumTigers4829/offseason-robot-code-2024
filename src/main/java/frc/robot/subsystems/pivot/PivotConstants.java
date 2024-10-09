ff2f403c41235f8efff157aa8b17be2057947d9d
Ari Davidson <ardavidson@students.chccs.k12.nc.us>
2024-10-09 13:11:00 -0400

    moved PivotConstants to subsystem and removed unnessecary variables

---
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.pivot;
+
+public class PivotConstants {
+  public static final double PIVOT_MASS = 0.0;
+  public static final double PIVOT_LENGTH = 0.0;
+  public static final double PIVOT_GEARING = 0.0;
+}
