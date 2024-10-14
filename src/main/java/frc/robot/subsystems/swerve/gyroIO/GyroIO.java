70c80c8b0b6aba8c682d91a0a79b55e8f0c27d12
Ishan <98932677+Ishan1522@users.noreply.github.com>
2024-10-14 17:18:13 -0400

    idk (#37)

--- src/main/java/frc/robot/subsystems/swerve/gyroIO/GyroIO.java
+++ src/main/java/frc/robot/subsystems/swerve/gyroIO/GyroIO.java
@@ -13,15 +13,12 @@ public interface GyroIO {
     public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
     public double[] accel = new double[] {0, 0, 0};
     public double yawVelocity = 0.0;
-    public boolean connected;
     public double[] odometryYawTimestamps = new double[] {};
-    public Rotation2d yawPosition;
-    public double yawVelocityRadPerSec;
   }
 
-  public default void updateInputs(GyroIOInputs inputs) {}
+  default void updateInputs(GyroIOInputs inputs) {}
 
-  public default void reset() {}
+  default void reset() {}
 
-  public default void addOffset(Rotation2d offset) {}
+  default void addOffset(Rotation2d offset) {}
 }
