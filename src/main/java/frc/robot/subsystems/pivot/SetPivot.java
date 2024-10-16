8e3b0f41f0da7ab032d00f2f51d7918ed9938e0d
NNLUliskey <ni.luke2011@gmail.com>
2024-10-16 17:50:55 -0400

    yay :D

---
+++ src/main/java/frc/robot/subsystems/pivot/SetPivot.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.pivot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SetPivot extends Command {
+  /** Creates a new SetPivot. */
+   private final Pivot pivot;
+  private final double angle;
+  public SetPivot(double angle, Pivot pivot) {
+    this.pivot = pivot;
+    this.angle = angle;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    pivot.setPivotAngle(angle);
+  
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
