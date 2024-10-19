6b9aae44791a439e9c311a74aa50d364a9e6ef59
NNLUliskey <ni.luke2011@gmail.com>
2024-10-19 11:13:52 -0400

    moved shooter roller command to commands instead of subsystem

--- src/main/java/frc/robot/commands/shooter/ManualShooterRoller.java
+++ src/main/java/frc/robot/commands/shooter/ManualShooterRoller.java
@@ -2,11 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.shooter;
+package frc.robot.commands.shooter;
 
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooter.Flywheel;
 
 public class ManualShooterRoller extends Command {
   private Flywheel flywheel;
