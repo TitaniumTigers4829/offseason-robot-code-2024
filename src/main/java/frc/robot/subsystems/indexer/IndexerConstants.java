fe268c868cfa19ba7bf3c68019aaf822c77ef9d0
Ishan1522 <iacharyagangopadhyay@students.chccs.k12.nc.us>
2024-11-03 10:56:11 -0500

    ok
    ok

--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.indexer;
-
-public final class IndexerConstants {
-  public static final double INDEXER_NEUTRAL_SPEED = 0.0;
-  public static final double INTAKE_SPEED = 0.8;
-  public static final double OUTTAKE_SPEED = 0 - 9;
-  public static final double INDEXER_PASS_SPEED = 0.1;
-}
