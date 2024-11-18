b0a1efdd9ac12c3c0332d3231f2da81f6be75dde
Ishan <98932677+Ishan1522@users.noreply.github.com>
2024-11-18 14:31:52 -0500

    stuffs

--- src/main/java/frc/robot/jni/ShooterTrajoptJNI.java
+++ src/main/java/frc/robot/jni/ShooterTrajoptJNI.java
@@ -1,33 +1,18 @@
-// Copyright (c) 2024 CurtinFRC
-// Open Source Software, you can modify it according to the terms
-// of the MIT License at the root of this project
-
 package frc.robot.jni;
 
-import edu.wpi.first.util.RuntimeLoader;
-import java.io.IOException;
-
 public class ShooterTrajoptJNI {
-  static RuntimeLoader<ShooterTrajoptJNI> loader = null;
-
   static {
     try {
-      loader =
-          new RuntimeLoader<>(
-              "ShooterTrajoptJNI",
-              RuntimeLoader.getDefaultExtractionRoot(),
-              ShooterTrajoptJNI.class);
-      loader.loadLibrary();
-    } catch (IOException e) {
+      System.loadLibrary("ShooterTrajoptJNI");
+    } catch (UnsatisfiedLinkError e) {
       e.printStackTrace();
       System.exit(1);
     }
   }
-
   /**
    * Calculate the optimal way to shoot a note to the speaker.
    *
-   * @param javatraj The Java array which will be filled with the angular velocity, yaw and pitch.
+   * @param javatraj The Java array which will be filled with the angular velocity, yaw, and pitch.
    * @param x The X position of the robot.
    * @param y The Y position of the robot.
    * @param vel_x The X velocity of the robot.
