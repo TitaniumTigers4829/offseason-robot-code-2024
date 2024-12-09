6af9077a9a1bef414b31e6e4321a4dc7af438452
Ishan <98932677+Ishan1522@users.noreply.github.com>
2024-12-09 14:41:16 -0500

    remove buildconstants from gitignore bc it is cooking CI

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "offseason-robot-code-2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 464;
+  public static final String GIT_SHA = "e85bebc1ff41f2682e3d4ccbe67f1f5ce0f8a0f3";
+  public static final String GIT_DATE = "2024-12-08 17:15:05 EST";
+  public static final String GIT_BRANCH = "add-aquila-constants";
+  public static final String BUILD_DATE = "2024-12-08 17:29:19 EST";
+  public static final long BUILD_UNIX_TIME = 1733696959570L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
